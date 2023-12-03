d3a6277c55d3397cb36c396a34867859834eaeaa
soumesh113 <soumesh.khare@gmail.com>
2023-12-03 13:06:20 +0530

    back

--- app/components/Navbaradmin.tsx
+++ app/components/Navbaradmin.tsx
@@ -18,97 +18,20 @@ export default async function Navbaradmin({ session }: { session: Session | null
     .eq('id', i)
     .single()
     const full_name = data?.full_name
-    const toggleMobileMenu = () => {
-        const mobileMenu = document.getElementById('mobile-menu');
-        if(mobileMenu)
-        mobileMenu.classList.toggle('hidden');
-      };
-    
-      const closeMobileMenu = () => {
-        const mobileMenu = document.getElementById('mobile-menu');
-        if(mobileMenu)
-        mobileMenu.classList.add('hidden');
-      };
-    
-      // Close the mobile menu when a link is clicked
-      const handleLinkClick = () => {
-        closeMobileMenu();
-      };
-    
-      return (
-        <nav className="w-full bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
-          <div className="container mx-auto flex justify-between items-center">
-            <Link href="/" className="text-white text-2xl font-bold">Blogs Website</Link>
-    
-            <div className="lg:hidden">
-              {/* Mobile menu button */}
-              <button
-                className="text-white focus:outline-none"
-                onClick={toggleMobileMenu}
-              >
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M4 6h16M4 12h16m-7 6h7"></path>
-                </svg>
-              </button>
+    return(
+        <nav className="w-full bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10 mr-4">
+           <div className="container mx-auto flex justify-between items-center">
+                <Link href="/" className="text-white text-2xl font-bold">Blogs Website</Link>
+                <div className="space-x-4">
+        <Link href="#compose" className="text-white">Create Blog</Link>
+        <Link href="#posts" className="text-white">View Blogs</Link>
+        <Link href="#acc" className="text-white">{full_name}</Link>
+        <Link href='/admin/' className="text-white">
+  Admin Panel
+</Link>
+      </div>
             </div>
-    
-            <div id="mobile-menu" className="lg:flex hidden space-x-4">
-              <Link href="#compose" className="text-white" onClick={handleLinkClick}>
-                Create Blog
-              </Link>
-              <Link href="#posts" className="text-white" onClick={handleLinkClick}>
-                View Blogs
-              </Link>
-              <Link href="#acc" className="text-white" onClick={handleLinkClick}>
-                {full_name}
-              </Link>
-              <Link href='/admin/' className="text-white" onClick={handleLinkClick}>
-                Admin Panel
-              </Link>
-            </div>
-          </div>
         </nav>
-      );
-    
-//     return(
-//         <nav className="w-full bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
-//     <div className="container mx-auto flex justify-between items-center">
-//         <Link href="/" className="text-white text-2xl font-bold">Blogs Website</Link>
-        
-//         <div className="lg:hidden">
-
-//             <button className="text-white focus:outline-none">
-//                 <svg
-//                     className="h-6 w-6"
-//                     fill="none"
-//                     strokeLinecap="round"
-//                     strokeLinejoin="round"
-//                     strokeWidth="2"
-//                     viewBox="0 0 24 24"
-//                     stroke="currentColor"
-//                 >
-//                     <path d="M4 6h16M4 12h16m-7 6h7"></path>
-//                 </svg>
-//             </button>
-//         </div>
-
-//         <div className="hidden lg:flex space-x-4">
-//             <Link href="#compose" className="text-white">Create Blog</Link>
-//             <Link href="#posts" className="text-white">View Blogs</Link>
-//             <Link href="#acc" className="text-white">{full_name}</Link>
-//             <Link href='/admin/' className="text-white">Admin Panel</Link>
-//         </div>
-//     </div>
-// </nav>
-
-//     )
+    )
     }
 }
