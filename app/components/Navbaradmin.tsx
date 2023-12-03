8fb63f7b7b12671479531382884824d08b0a819c
soumesh113 <soumesh.khare@gmail.com>
2023-12-03 13:01:26 +0530

    mobile3

--- app/components/Navbaradmin.tsx
+++ app/components/Navbaradmin.tsx
@@ -4,8 +4,6 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase";
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 export default async function Navbaradmin({ session }: { session: Session | null })
 {
     'use server';
@@ -20,69 +18,66 @@ export default async function Navbaradmin({ session }: { session: Session | null
     .eq('id', i)
     .single()
     const full_name = data?.full_name
-    const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      // Close the mobile menu when a new route is navigated to
-      closeMobileMenu();
-    };
-
-    // Attach the event listener for route changes
-    router.events.on('routeChangeStart', handleRouteChange);
-
-    // Cleanup the event listener when the component unmounts
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, []);
-
-  const toggleMobileMenu = () => {
-    const mobileMenu = document.getElementById('mobile-menu');
-    if(mobileMenu)
-    mobileMenu.classList.toggle('hidden');
-  };
-
-  const closeMobileMenu = () => {
-    const mobileMenu = document.getElementById('mobile-menu');
-    if(mobileMenu)
-    mobileMenu.classList.add('hidden');
-  };
-
-  return (
-    <nav className="w-full bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-white text-2xl font-bold">Blogs Website</Link>
-
-        <div className="lg:hidden">
-          {/* Mobile menu button */}
-          <button
-            className="text-white focus:outline-none"
-            onClick={toggleMobileMenu}
-          >
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
-          </button>
-        </div>
-
-        <div id="mobile-menu" className="lg:flex hidden space-x-4">
-          <Link href="#compose" className="text-white">Create Blog</Link>
-          <Link href="#posts" className="text-white">View Blogs</Link>
-          <Link href="#acc" className="text-white">{full_name}</Link>
-          <Link href='/admin/' className="text-white">Admin Panel</Link>
-        </div>
-      </div>
-    </nav>
-  );
+    const toggleMobileMenu = () => {
+        const mobileMenu = document.getElementById('mobile-menu');
+        if(mobileMenu)
+        mobileMenu.classList.toggle('hidden');
+      };
+    
+      const closeMobileMenu = () => {
+        const mobileMenu = document.getElementById('mobile-menu');
+        if(mobileMenu)
+        mobileMenu.classList.add('hidden');
+      };
+    
+      // Close the mobile menu when a link is clicked
+      const handleLinkClick = () => {
+        closeMobileMenu();
+      };
+    
+      return (
+        <nav className="w-full bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+          <div className="container mx-auto flex justify-between items-center">
+            <Link href="/" className="text-white text-2xl font-bold">Blogs Website</Link>
+    
+            <div className="lg:hidden">
+              {/* Mobile menu button */}
+              <button
+                className="text-white focus:outline-none"
+                onClick={toggleMobileMenu}
+              >
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+              </button>
+            </div>
+    
+            <div id="mobile-menu" className="lg:flex hidden space-x-4">
+              <Link href="#compose" className="text-white" onClick={handleLinkClick}>
+                Create Blog
+              </Link>
+              <Link href="#posts" className="text-white" onClick={handleLinkClick}>
+                View Blogs
+              </Link>
+              <Link href="#acc" className="text-white" onClick={handleLinkClick}>
+                {full_name}
+              </Link>
+              <Link href='/admin/' className="text-white" onClick={handleLinkClick}>
+                Admin Panel
+              </Link>
+            </div>
+          </div>
+        </nav>
+      );
+    
 //     return(
 //         <nav className="w-full bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
 //     <div className="container mx-auto flex justify-between items-center">
