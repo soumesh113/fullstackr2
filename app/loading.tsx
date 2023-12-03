fb715a3098236bd7b33875fd7c554c36d2101c76
soumesh113 <soumesh.khare@gmail.com>
2023-12-03 15:30:41 +0530

    load

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ export default function Loading(){
     return(
         <main>
             <div className="flex items-center justify-center h-screen">
-            <h1 className="animate text-5xl">Loading</h1>
+            <h1 className="animate text-5xl">Loading...</h1>
             </div>
         </main>
     )
